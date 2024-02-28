17f486d3efda7960d7706b3314306e6392f788b6
abenav4 <abenav.vijayakumaran@mail.utoronto.ca>
2024-02-27 23:36:02 -0500

    Quartz sync: Feb 27, 2024, 11:36 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 gigagaming TAGALOG",
+    pageTitle: "૮ • ﻌ • ა 🇵🇭 gigagaming TAGALOG",
     enableSPA: true,
     enablePopovers: true,
     analytics: {

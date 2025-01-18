3d567fbc328883b48deaaf66be791f14aef26729
abenav4 <abenav.vijayakumaran@mail.utoronto.ca>
2025-01-17 20:22:11 -0500

    Quartz sync: Jan 17, 2025, 8:22 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🏺 abenav ",
+    pageTitle: "🪔 abenav ",
     enableSPA: true,
     enablePopovers: true,
     analytics: {

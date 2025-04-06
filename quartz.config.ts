81f7281db3ae816b66b9d049d4d9a5e79da633c4
Abenav <abenav123@gmail.com>
2025-04-05 21:48:18 -0400

    Quartz sync: Apr 5, 2025, 9:48 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪔 abenav ", // coconut?
+    pageTitle: "🪷 abenav ", // coconut?
     enableSPA: true,
     enablePopovers: true,
     analytics: {
